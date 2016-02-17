effba8e6187f9271e9eadc5f6a16131480d2039c
ofer515 <oferikko97@gmail.com>
2016-02-17 17:37:56 +0200

    Gui implementation

--- programs_finder.py
+++ programs_finder.py
@@ -1,62 +1,54 @@
 import StringIO
 import traceback
 import wmi
-import win32com.shell.shell as shell
 import sys
-import os
-
 from _winreg import (HKEY_LOCAL_MACHINE, KEY_ALL_ACCESS, 
                      OpenKey, EnumValue, QueryValueEx)
 
-""""
-privs = 'asadmin'
-print sys.argv[0]
-if sys.argv[-1] != privs:
-    script = os.path.abspath(sys.argv[0])
-    params = ' '.join([script] + sys.argv[1:] + [privs])
-    shell.ShellExecuteEx(lpVerb='runas', lpFile=sys.executable, lpParameters=params)
-"""
 
+def main():
+    new_file = open('programs_list.log', 'w')
+    errorLog = open('errors.log', 'w')
+    print "hi"
+    r = wmi.Registry()
+    result, names = r.EnumKey(hDefKey=HKEY_LOCAL_MACHINE, sSubKeyName=r"Software\Microsoft\Windows\CurrentVersion\Uninstall")
 
-
-prog_list = open('programs_list.log', 'w')
-errorLog = open('errors.log', 'w')
-
-
-r = wmi.Registry()
-result, names = r.EnumKey(hDefKey=HKEY_LOCAL_MACHINE, sSubKeyName=r"Software\Microsoft\Windows\CurrentVersion\Uninstall")
- 
-prog_list.write('These subkeys are found under "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall"\n\n')
-errorLog.write("Errors\n\n")
-separator = "*" * 80
-keyPath = r"Software\Microsoft\Windows\CurrentVersion\Uninstall"
-for subkey in names:
-    try:
-        prog_list.write(separator + '\n\n')
-        path = keyPath + "\\" + subkey
-        key = OpenKey(HKEY_LOCAL_MACHINE, path, 0, KEY_ALL_ACCESS) 
+    new_file.write('These subkeys are found under "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Uninstall"\n\n')
+    errorLog.write("Errors\n\n")
+    separator = "*" * 80
+    keyPath = r"Software\Microsoft\Windows\CurrentVersion\Uninstall"
+    for subkey in names:
         try:
-            temp = QueryValueEx(key, 'DisplayName')
-            display = str(temp[0])
-            if display is not "":
-                prog_list.write('Display Name: ' + display + '\nRegkey: ' + subkey + '\n')
-            else:
+            new_file.write(separator + '\n\n')
+            path = keyPath + "\\" + subkey
+            key = OpenKey(HKEY_LOCAL_MACHINE, path, 0, KEY_ALL_ACCESS)
+            try:
+                temp = QueryValueEx(key, 'DisplayName')
+                display = str(temp[0])
+                if display is not "":
+                    new_file.write('Display Name: ' + display + '\nRegkey: ' + subkey + '\n')
+                else:
+                    pass
+            except:
+                new_file.write('Regkey: ' + subkey + '\n')
+
+        except Exception as e:
+            print e
+            fp = StringIO.StringIO()
+            traceback.print_exc(file=fp)
+            errorMessage = fp.getvalue()
+            try:
+                error = 'Error for ' + key + '. Message follows:\n' + errorMessage
+                errorLog.write(error)
+                errorLog.write("\n\n")
+            except:
                 pass
-        except:
-            prog_list.write('Regkey: ' + subkey + '\n')
- 
-    except Exception as e:
-        print e
-        fp = StringIO.StringIO()
-        traceback.print_exc(file=fp)
-        errorMessage = fp.getvalue()
-        try:
-            error = 'Error for ' + key + '. Message follows:\n' + errorMessage
-            errorLog.write(error)
-            errorLog.write("\n\n")
-        except:
-            pass
-print "All programs that was needed are found"
-print len(names)
-prog_list.close()
-errorLog.close()
+    print "All programs that was needed are found"
+    print len(names)
+    new_file.close()
+    errorLog.close()
+    sys.exit(0)
+if __name__ == "__main__":
+    main()
+else:
+    print __name__
--- start.py
+++ start.py
@@ -1,3 +1,31 @@
+from Tkinter import *
+import win32com.shell.shell as shell
+import os
+
+
+def main_window():
+    window = Tk()
+    l = Label(text="Personal helper!")
+    b = Button(window, text="start scanning for programs", command =scan_computer_programs)
+    l.pack()
+    b.pack()
+    window.mainloop()
+
+def scan_computer_programs():
+    privs = 'asadmin'
+    filename = "programs_finder.py"
+    if sys.argv[-1] != privs:
+        script = os.path.dirname(filename) + os.path.basename(filename)
+        print script
+        params = ' '.join([script] + sys.argv[1:] + [privs])
+        try:
+            shell.ShellExecuteEx(lpVerb='runas', lpFile=sys.executable, lpParameters=params)
+        except Exception as e:
+            print e
+            print "need admin"
 
 
 
+
+if __name__ == "__main__":
+    main_window()
