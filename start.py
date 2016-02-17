55135d7ea6e006adb1e4ae3ac494f1bf364f84bd
ofer515 <oferikko97@gmail.com>
2016-02-17 23:15:49 +0200

    small ui updates
    icons and size changes

--- start.py
+++ start.py
@@ -5,8 +5,11 @@ import os
 
 def main_window():
     window = Tk()
+    window.geometry("500x500")
+    window.iconbitmap(r"C:\Users\ofer\Desktop\github files\project\Alien_robot.ico")
+    window.wm_title("Personal Helper!")
     l = Label(text="Personal helper!")
-    b = Button(window, text="start scanning for programs", command =scan_computer_programs)
+    b = Button(window, text="start scanning for programs", command=scan_computer_programs)
     l.pack()
     b.pack()
     window.mainloop()
